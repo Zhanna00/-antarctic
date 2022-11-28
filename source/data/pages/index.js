f5a8c2a21186d011ef6d9a671162d7f3b4e80ffa
Zhanna <janna500116@gmail.com>
2022-11-29 01:42:21 +0700

    Шапка

--- source/data/pages/index.js
+++ source/data/pages/index.js
@@ -1,2 +1,15 @@
+import html from '../../js/utils/html.js';
+
 export default {
+  promo: {
+    content: html`<h1>Круизы в Антарктику</h1>
+      <p>Волшебный мир айсбергов и пингвинов</p>`,
+    image: {
+      default: 'img/firstscreen.jpg',
+      default2x: 'img/firstscreen@2x.jpg',
+      immediately: true,
+      webp: 'img/firstscreen.webp',
+      webp2x: 'img/firstscreen@2x.webp',
+    },
+  },
 };
